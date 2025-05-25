fccc15a1205a8fdad8f63522723a2eabfa41819a
Awomiah <priscillamensah4000@gmail.com>
2025-05-25 14:50:28 +0000

    Adding conditional sentence

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+let argument = 2;
+if (argument= 0) {
+    console.log("no argument");
+}
+else if (argument = 1) {
+    console.log("Argument found");
+}
+else {
+    console.log("Arguments found");
+}
