f23cb3f415b897cca7e85bf49235c9a6389a734b
Awomiah <priscillamensah4000@gmail.com>
2025-05-25 17:03:40 +0000

    Add 3-value arguments

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,10 +1,7 @@
 const args = process.argv.slice(2);
  
 if (args[0] !== undefined) {
-    console.log("One argument");
-}
-else if (args[0,1] !== undefined) {
-    console.log("2 arguments");
+    console.log([0]);
 }
 else {
     console.log("No argument");
