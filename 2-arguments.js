858d44421c127b031bdc3b2fba9222176ff360f5
Awomiah <priscillamensah4000@gmail.com>
2025-05-25 15:01:17 +0000

    Adding conditional sentence

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,9 @@
-let argument = 2;
-if (argument= 0) {
-    console.log("no argument");
-}
-else if (argument = 1) {
-    console.log("Argument found");
-}
-else {
-    console.log("Arguments found");
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
