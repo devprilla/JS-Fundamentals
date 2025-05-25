a4b2293996a0ac6731e6cc66b0de44173205e3fb
Awomiah <priscillamensah4000@gmail.com>
2025-05-25 18:42:56 +0000

    Add 4-concat

--- 4-concat.js
+++ 4-concat.js
@@ -3,7 +3,7 @@ const args = process.argv.slice(2);
 args[0] = "Python";
 args[1] = "fun";
 args[2] = null;
-if(args[0] , args[2] && args[1] !== undefined) {
+if(args[0] && args[1] !== undefined) {
     console.log(args[0] + " is " + args[1]);
     console.log(args[0]);
 
